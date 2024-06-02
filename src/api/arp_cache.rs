43a1cb0dfa2842966bd5844d1665a51b8050831f
Louis Bignon <louisbignon1@gmail.com>
2024-06-02 19:22:33 +0200

    network verification test done

--- src/api/arp_cache.rs
+++ src/api/arp_cache.rs
@@ -4,22 +4,23 @@ use std::io;
 use std::net::{IpAddr, Ipv4Addr};
 use std::ops::Sub;
 use std::time::{Duration,Instant};
-use pnet::ipnetwork::{IpNetwork,Ipv4Network};
+use pnet::ipnetwork::Ipv4Network;
 use pnet::packet::arp::{Arp, ArpOperations};
 use pnet::datalink::NetworkInterface;
 use pnet::packet::ipv4;
 
 use super::layer_3::Layer3Infos;
+use super::packet_infos;
 
 pub struct ArpCache{
     cache: HashMap<Ipv4Addr,(String,Instant)>,
     ttl: Duration,
-    interface : NetworkInterface,
+    interface : Ipv4Network,
 }
 
 impl ArpCache{
 
-    pub fn new(ttl: Duration, interface : NetworkInterface) -> Self {
+    pub fn new(ttl: Duration, interface : Ipv4Network) -> Self {
         ArpCache{
             cache : HashMap::new(),
             ttl : ttl,
@@ -53,43 +54,41 @@ impl ArpCache{
     //     // Ajout de le pair et vérification doublon -> Arp Reply
     //     // Endpoint une adresse mac valide (pas broadcast par exemple) -> Arp Reply
     // }
-
     fn network_verification(&mut self, packet : &PacketInfos) -> Result<(),String>{
         //get subnet of network interface
         match packet.get_layer_3_infos() {
             Layer3Infos::ARP(arp_handler) => {
-                let ip_network = self.interface.ips[0];
-                if ip_network.is_ipv4(){
-                    match arp_handler.operation {
-                        ArpOperations::Reply => {
-                            if arp_handler.ip_source.is_broadcast() || arp_handler.ip_source.is_loopback(){
-                                return Err("IP source cannot be broadcast adress".to_string())
-                            }
-                            else if arp_handler.hw_source.is_broadcast() || arp_handler.hw_source.is_local(){
-                                return Err("handle alert".to_string())
-                            }
-                            else{
-                                return Ok(())
-                            }
-                        }   
-                        ArpOperations::Request => {
-                            if arp_handler.ip_source.is_broadcast() || arp_handler.ip_source.is_loopback(){
-                                return Err("IP source cannot be broadcast adress".to_string())
-                            }
-                            else if !(ip_network.contains(IpAddr::from(arp_handler.ip_source))){
-                                return Err("Ip source not in subnet".to_string());
-                            }
-                            else if !(arp_handler.hw_source.eq(packet.get_sender_hw_addr())){
-                                return Err("handle alert -> mac address Ip packet do not correspond to mac address ARP packet".to_string())
-                            }
-                            else{
-                                return Ok(())
-                            }
+                let ip_network = self.interface;
+                match arp_handler.operation {
+                    ArpOperations::Reply => {
+                        if arp_handler.ip_source.is_broadcast() || arp_handler.ip_source.is_loopback(){
+                            return Err("IP source cannot be broadcast address".to_string())
                         }
-                        _ => {return Err("ARP error".to_string())}
+                        else if !(ip_network.contains(arp_handler.ip_source)){
+                            return Err("Ip source not in subnet".to_string());
+                        }
+                        else if arp_handler.hw_source.is_broadcast() || arp_handler.hw_source.is_local(){
+                            return Err("handle alert".to_string())
+                        }
+                        else{
+                            return Ok(())
+                        }
+                    }   
+                    ArpOperations::Request => {
+                        if arp_handler.ip_source.is_broadcast() || arp_handler.ip_source.is_loopback(){
+                            return Err("IP source cannot be broadcast address".to_string())
+                        }
+                        else if !(ip_network.contains(arp_handler.ip_source)){
+                            return Err("Ip source not in subnet".to_string());
+                        }
+                        else if !(arp_handler.hw_source.eq(packet.get_sender_hw_addr())){
+                            return Err("handle alert -> mac address Ip packet do not correspond to mac address ARP packet".to_string())
+                        }
+                        else{
+                            return Ok(())
+                        }
                     }
-                }else {
-                    return Err("Cannot use IPV6 address".to_string())
+                    _ => {return Err("ARP error".to_string())}
                 }
             }
             _ => {return Err("Not an ARP packet".to_string())}
@@ -109,43 +108,125 @@ mod tests{
     use super::*;
     use std::time::Duration;
 
-    use pnet::{datalink::dummy::dummy_interface, packet::{arp::{ArpHardwareType, ArpHardwareTypes, ArpOperations, MutableArpPacket}, ethernet::{EtherTypes, EthernetPacket, MutableEthernetPacket}, Packet}};
+    use pnet::{packet::{arp::{ArpHardwareTypes, ArpOperations, MutableArpPacket}, ethernet::{EtherTypes, MutableEthernetPacket}}, util::MacAddr};
     use crate::api::packet_infos::PacketInfos;
 
-    #[test]
-    fn test_network_verification(){
-        // Définissez les adresses MAC source et de destination
-        let mut ethernet_buffer = [0u8; 42];
-        let mut ethernet_packet = MutableEthernetPacket::new(&mut ethernet_buffer).unwrap();
-        
-        ethernet_packet.set_destination([0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF].into());
-        ethernet_packet.set_source([0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC].into());
+
+    fn paquet_generator<'a>(ether_mac_source : MacAddr,arp_mac_source : MacAddr,  mac_dest : MacAddr , ip_source : Ipv4Addr, ip_dest : Ipv4Addr,  ethernet_packet : &mut MutableEthernetPacket,  arp_packet : &mut MutableArpPacket){
+
+        ethernet_packet.set_destination(mac_dest);
+        ethernet_packet.set_source(ether_mac_source);
         ethernet_packet.set_ethertype(EtherTypes::Arp);
 
-        let mut arp_buffer = [0u8,28];
-        let mut arp_packet = MutableArpPacket::new(&mut arp_buffer).unwrap();
         arp_packet.set_hardware_type(ArpHardwareTypes::Ethernet);
         arp_packet.set_protocol_type(EtherTypes::Ipv4); // IPv4
         arp_packet.set_hw_addr_len(6);
         arp_packet.set_proto_addr_len(4);
         arp_packet.set_operation(ArpOperations::Request);
-        arp_packet.set_sender_hw_addr([0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC].into());
-        arp_packet.set_sender_proto_addr([192, 168, 1, 1].into());
+        arp_packet.set_sender_hw_addr(arp_mac_source);
+        arp_packet.set_sender_proto_addr(ip_source);
         arp_packet.set_target_hw_addr([0; 6].into()); // Target MAC
-        arp_packet.set_target_proto_addr([192, 168, 1, 2].into()); // Target IP
+        arp_packet.set_target_proto_addr(ip_dest); // Target IP
+
+    }
 
 
-        //copy du paquet arp dans le payload du packet ethernet
-        let mut ethernet_payload = ethernet_packet.set_payload(&arp_buffer);
-
-
+    #[test]
+    fn test_network_verification(){
+        
+        
         //cache arp
-        let interface = dummy_interface(1);
+        let interface : Ipv4Network = "192.168.1.1/24".parse().unwrap(); 
         let mut cache = ArpCache::new(Duration::new(50, 0), interface);
         let interface_name = String::from("eth1");
-        let fake_paquet_info : PacketInfos = PacketInfos::new(&interface_name, &ethernet_packet.to_immutable());
+        
+        // Définissez les adresses MAC source et de destination
+        let mut ethernet_buffer  = [0u8; 42];
+        let mut arp_buffer = [0u8;28];
+
+        {
+            let mut arp_packet: MutableArpPacket = MutableArpPacket::new(&mut arp_buffer).unwrap();
+            let mut ethernet_packet : MutableEthernetPacket = MutableEthernetPacket::new(&mut ethernet_buffer).unwrap();
+    
+            paquet_generator(
+                [0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC].into(),
+                [0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC].into(), 
+                [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF].into(), 
+                [192, 168, 1, 2].into(), 
+                [192, 168, 1, 3].into(),
+                &mut ethernet_packet,
+                &mut arp_packet);
+    
+            ethernet_packet.set_payload(&arp_buffer);
+    
+    
+            let fake_paquet_info : PacketInfos = PacketInfos::new(&interface_name, &ethernet_packet.to_immutable());
+            assert_eq!(cache.network_verification(&fake_paquet_info),Ok(()));
+        }
+
+        {
+            let mut arp_packet: MutableArpPacket = MutableArpPacket::new(&mut arp_buffer).unwrap();
+            let mut ethernet_packet : MutableEthernetPacket = MutableEthernetPacket::new(&mut ethernet_buffer).unwrap();
+    
+            paquet_generator(
+                [0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC].into(),
+                [0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC].into(), 
+                [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF].into(), 
+                [255,255,255,255].into(), 
+                [192, 168, 1, 3].into(),
+                &mut ethernet_packet,
+                &mut arp_packet);
+    
+            ethernet_packet.set_payload(&arp_buffer);
+    
+    
+            let fake_paquet_info : PacketInfos = PacketInfos::new(&interface_name, &ethernet_packet.to_immutable());
+            assert_eq!(cache.network_verification(&fake_paquet_info),Err("IP source cannot be broadcast address".to_string()));
+        }
 
 
-        assert_eq!(cache.network_verification(&fake_paquet_info),Ok(()));
+        {
+            let mut arp_packet: MutableArpPacket = MutableArpPacket::new(&mut arp_buffer).unwrap();
+            let mut ethernet_packet : MutableEthernetPacket = MutableEthernetPacket::new(&mut ethernet_buffer).unwrap();
+    
+            paquet_generator(
+                [0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC].into(),
+                [0x12, 0x34, 0x56, 0x78, 0x9A, 0xBF].into(),
+                [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF].into(), 
+                [192, 168, 1, 2].into(), 
+                [192, 168, 1, 3].into(),
+                &mut ethernet_packet,
+                &mut arp_packet);
+    
+            ethernet_packet.set_payload(&arp_buffer);
+    
+    
+            let fake_paquet_info : PacketInfos = PacketInfos::new(&interface_name, &ethernet_packet.to_immutable());
+            assert_eq!(cache.network_verification(&fake_paquet_info),Err("handle alert -> mac address Ip packet do not correspond to mac address ARP packet".to_string()));
+        }
+
+        {
+            let mut arp_packet: MutableArpPacket = MutableArpPacket::new(&mut arp_buffer).unwrap();
+            let mut ethernet_packet : MutableEthernetPacket = MutableEthernetPacket::new(&mut ethernet_buffer).unwrap();
+    
+            paquet_generator(
+                [0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC].into(),
+                [0x12, 0x34, 0x56, 0x78, 0x9A, 0xBC].into(), 
+                [0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF].into(), 
+                [178, 16, 1, 2].into(), 
+                [192, 168, 1, 3].into(),
+                &mut ethernet_packet,
+                &mut arp_packet);
+    
+            ethernet_packet.set_payload(&arp_buffer);
+    
+    
+            let fake_paquet_info : PacketInfos = PacketInfos::new(&interface_name, &ethernet_packet.to_immutable());
+            assert_eq!(cache.network_verification(&fake_paquet_info),Err("Ip source not in subnet".to_string()));
+        }
+        
+
+
+
     }
 }
